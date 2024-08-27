d9098316fd29155e58fcf59797e88147ceb53ac7
bdiaz-dev <144321133+bdiaz-dev@users.noreply.github.com>
2024-08-27 08:17:08 +0200

    background editing

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import MenuLayout from './components/nav/MenuLayout';
 import todayDate from './libs/todayDate';
 import PageHead from './components/helmet/PageHead';
 import LoadingMessage from './components/loading/LoadingMessage';
+import BackgroundVideo from './components/background/Background';
 
 function App() {
   const { lang } = useLanguage();
@@ -30,6 +31,7 @@ function App() {
 
   return (
     <>
+    <BackgroundVideo />
       {/* DYNAMIC TITLE AND FAVICON */}
       {
         weatherData?.main
